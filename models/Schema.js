c64765d9241d3680d88d024287f09c5b1cfc7bee
mohit nandan <mohitnandan81825@gmail.com>
2024-06-30 23:41:22 +0530

    create account schema

--- models/Schema.js
+++ models/Schema.js
@@ -30,9 +30,22 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
 // Create a model from the schema
+const Account = mongoose.model('Account', accountSchema);
 const User = mongoose.model('User', userSchema);
 
 module.exports = {
-	User
+	User, Account,
 };

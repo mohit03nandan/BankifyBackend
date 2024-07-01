13126d69742ea120fa33272765458a8d922800c7
mohit nandan <mohitnandan81825@gmail.com>
2024-07-01 21:37:59 +0530

    add balance routes in account.js

--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,17 @@
 const express = require('express');
-
+const {Account} = require('../models/Schema');
+const {authMiddleware} = require('../middleware/authMiddleware');
 const router = express.Router();
+
+
+router.get("/balance", authMiddleware, async (req, res) => {
+    const account = await Account.findOne({
+        userId: req.userId
+    });
+
+    res.json({
+        balance: account.balance
+    })
+});
 
 module.exports = router;
